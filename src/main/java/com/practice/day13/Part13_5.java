c399b974a334fd97a08be97055a799e0d7a25a46
jsbmomo <tytyes40@gmail.com>
2025-03-01 00:02:51 +0900

    12일차 리스트 part 5, n개 간격의 원소들

---
+++ src/main/java/com/practice/day13/Part13_5.java
@@ -0,0 +1,29 @@
+package com.practice.day13;
+
+import java.util.Arrays;
+
+public class Part13_5 {
+
+  private static int[] solution(int[] num_list, int n) {
+    int size = (num_list.length + n - 1) / n;
+    int[] answer = new int[(num_list.length / n) + 1];
+    int index = 0;
+
+    for (int i = 0; i < num_list.length; i += n) {
+      if (n > num_list.length)
+        break;
+
+      answer[index++] = num_list[i];
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int[] values = new int[] { 4, 2, 6, 1, 7, 6 };
+    int num = 2;
+
+    Arrays.stream(solution(values, num)).forEach(System.out::println);
+  }
+
+}

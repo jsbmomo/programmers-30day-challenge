bf86854beaa5ce9f5f365074bc2ab2ed98e56477
jsbmomo <tytyes40@gmail.com>
2025-03-01 00:03:51 +0900

    13일차 리스트 Part5, n개 간격의 원소들 오타 수정

--- src/main/java/com/practice/day13/Part13_5.java
+++ src/main/java/com/practice/day13/Part13_5.java
@@ -6,7 +6,7 @@ public class Part13_5 {
 
   private static int[] solution(int[] num_list, int n) {
     int size = (num_list.length + n - 1) / n;
-    int[] answer = new int[(num_list.length / n) + 1];
+    int[] answer = new int[size];
     int index = 0;
 
     for (int i = 0; i < num_list.length; i += n) {

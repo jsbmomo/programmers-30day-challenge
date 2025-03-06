6dcbfc5fd41f16e11f66fcd25b1bcc6c393a3dec
jsbmomo <tytyes40@gmail.com>
2025-03-07 00:11:42 +0900

    16일차 문자열 part 1~2, 대소문자 변환

---
+++ src/main/java/com/practice/day16/Part16_2.java
@@ -0,0 +1,15 @@
+package com.practice.day16;
+
+public class Part16_2 {
+
+  private static String solution(String myString) {
+    return myString.toLowerCase();
+  }
+
+  public static void main(String[] args) {
+    String myString = "aBcDeFg";
+
+    System.out.println(solution(myString));
+  }
+
+}

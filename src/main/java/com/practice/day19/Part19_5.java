046c2bf2c70322b94ef25c5ac1eaaaa1944e7e99
jsbmomo <tytyes40@gmail.com>
2025-03-19 23:58:58 +0900

    19일차 문자열, 리스트 part 5, 무작위로 K개의 수 뽑기

---
+++ src/main/java/com/practice/day19/Part19_5.java
@@ -0,0 +1,35 @@
+package com.practice.day19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Part19_5 {
+
+  private static int[] solution(int[] arr, int k) {
+    ArrayList<Integer> answer = new ArrayList<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (answer.size() >= k)
+        break;
+
+      if (!answer.contains(arr[i]))
+        answer.add(arr[i]);
+    }
+
+    if (answer.size() < k) {
+      for (int i = answer.size() - 1; i < k; i++) {
+        answer.add(-1);
+      }
+    }
+
+    return answer.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[] { 0, 1, 1, 2, 2, 3 };
+    int k = 3;
+
+    Arrays.stream(solution(arr, k)).forEach(System.out::println);
+  }
+
+}
